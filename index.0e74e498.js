e65fcf5eb95fe76676ff566077b352eddb8d98bc
SergY29 <106399401+SergY29@users.noreply.github.com>
2022-08-12 10:35:44 +0000

    Deploying to gh-pages from @ SergY29/team14-project@9970f590355b514b030001798dd29f9b5364aca9 🚀

---
+++ index.0e74e498.js
@@ -0,0 +1,2 @@
+var buttons=document.querySelectorAll(".product__item button");function flipCard(t){t.currentTarget.closest(".product__item").classList.toggle("is-flipped")}buttons.forEach((function(t){return t.addEventListener("click",flipCard)}));
+//# sourceMappingURL=index.0e74e498.js.map
