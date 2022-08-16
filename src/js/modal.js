a3299a378d6399cd966bedc6f384b3d0da57dfaa
LubomirDD <96430493+LubomirDD@users.noreply.github.com>
2022-08-17 01:50:05 +0300

    new add

--- src/js/modal.js
+++ src/js/modal.js
@@ -16,22 +16,6 @@
 
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-2-open]'),
-    closeModalBtn: document.querySelector('[data-modal-2-close]'),
-    modal: document.querySelector('[data-modal-2]'),
-  };
-
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-
-  function toggleModal() {
-    document.body.classList.toggle('modal-2-open');
-    refs.modal.classList.toggle('is-hidden');
-  }
-})();
-
-(() => {
-  const refs = {
     openModalBtn: document.querySelector('[data-modal-mobile-open]'),
     modal: document.querySelector('[data-modal]'),
   };
