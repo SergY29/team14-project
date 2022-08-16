550ba16afa108785da6276b5a127659b3a1f4e17
SergY29 <106399401+SergY29@users.noreply.github.com>
2022-08-16 19:52:55 +0000

    Deploying to gh-pages from @ SergY29/team14-project@1d8ba35903cfff31452ad6f0f3f183bf80c2770b 🚀

--- index.ce44c653.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openMobileMenuBtn:document.querySelector("[data-mobile-menu-open]"),closeMobileMenuBtn:document.querySelector("[data-mobile-menu-close]"),mobileMenu:document.querySelector("[data-mobile-menu]")};function n(){document.body.classList.toggle("modal-open"),e.mobileMenu.classList.toggle("is-hidden")}e.openMobileMenuBtn.addEventListener("click",n),e.closeMobileMenuBtn.addEventListener("click",n)}();
-//# sourceMappingURL=index.ce44c653.js.map
---
+++ index.f6fd1042.js
@@ -0,0 +1,2 @@
+!function(){var e={openMobileMenuBtn:document.querySelector("[data-mobile-menu-open]"),closeMobileMenuBtn:document.querySelector("[data-mobile-menu-close]"),mobileMenu:document.querySelector("[data-mobile-menu]"),mobileList:document.querySelector(".phone-list")};function n(){document.body.classList.toggle("modal-open"),e.mobileMenu.classList.toggle("is-hidden")}e.openMobileMenuBtn.addEventListener("click",n),e.mobileList.addEventListener("click",n),e.closeMobileMenuBtn.addEventListener("click",n)}();
+//# sourceMappingURL=index.f6fd1042.js.map
