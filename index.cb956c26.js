5a56c2cd049f409e18a49c17b8e30d333155ade9
SergY29 <106399401+SergY29@users.noreply.github.com>
2022-08-17 08:50:02 +0000

    Deploying to gh-pages from @ SergY29/team14-project@ae7ac99c71c794afd670ae8f3ac419815dbb4d3e 🚀

---
+++ index.cb956c26.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[data-modal-how-open]"),closeModalBtn:document.querySelector("[data-modal-how-close]"),modal:document.querySelector("[data-modal-how]")};function e(){document.body.classList.toggle("modal-open"),o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.cb956c26.js.map
