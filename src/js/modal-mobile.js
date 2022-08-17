d2fa466a43df132b34e524b1b2ae897dec2c854e
SergeyYurtin <106399401+SergY29@users.noreply.github.com>
2022-08-17 16:56:16 +0300

    fixed

--- src/js/modal-mobile.js
+++ src/js/modal-mobile.js
@@ -21,7 +21,7 @@
   // Close the mobile menu on wider screens if the device orientation changes
   window.matchMedia("(min-width: 768px)").addEventListener("change", (e) => {
     if (!e.matches) return;
-    mobileMenu.classList.remove("is-mobile");
+    mobileMenu.classList.remove("is-open");
     openMenuBtn.setAttribute("aria-expanded", false);
     bodyScrollLock.enableBodyScroll(document.body);
   });
