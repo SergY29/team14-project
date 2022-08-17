eccfa411647b1457f98a718f28bb48ff635e0068
SergY29 <106399401+SergY29@users.noreply.github.com>
2022-08-17 08:52:24 +0000

    Deploying to gh-pages from @ SergY29/team14-project@18d6eaa81fbdb401efa25a5758d115e3cfee12ad 🚀

---
+++ index.a8df1bd8.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-franchise-open]"),closeModalBtn:document.querySelector("[data-modal-franchise-close]"),modal:document.querySelector("[data-modal-franchise]")};function o(){document.body.classList.toggle("modal-open"),e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.a8df1bd8.js.map
