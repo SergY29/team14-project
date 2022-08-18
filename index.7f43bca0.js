ba4e85cc81dc8c2dd199eb93d5f664522d1cd97b
SergY29 <106399401+SergY29@users.noreply.github.com>
2022-08-18 17:12:22 +0000

    Deploying to gh-pages from @ SergY29/team14-project@c9f574eae3cc5d744b4f290f73ae5083af959359 🚀

---
+++ index.7f43bca0.js
@@ -0,0 +1,2 @@
+jQuery(document).ready((function(){var o=$("#button");$(window).scroll((function(){$(window).scrollTop()>300?o.addClass("show"):o.removeClass("show")})),o.on("click",(function(o){o.preventDefault(),$("html, body").animate({scrollTop:0},"300")}))}));
+//# sourceMappingURL=index.7f43bca0.js.map
